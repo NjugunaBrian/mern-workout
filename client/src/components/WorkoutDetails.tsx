2b67f3afd9f63f3d995fa1d37fddabeae6ee10f8
NjugunaBrian <bryunnjuguna300@gmail.com>
2024-07-08 19:24:50 +0300

    feat: updating global state using delete

--- client/src/components/WorkoutDetails.tsx
+++ client/src/components/WorkoutDetails.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useWorkoutsContext } from "../hooks/useWorkoutContext";
 
 interface WorkoutProps {
     createdAt?: string | Date;
@@ -9,12 +10,31 @@ interface WorkoutProps {
 }
 
 function WorkoutDetails({ workout }: { workout: WorkoutProps}): JSX.Element{
+
+    const { dispatch } = useWorkoutsContext();
+
+    const handleDelete = async() => {
+        const response = await fetch('/api/workouts/' + workout._id, {
+            method: 'DELETE',
+        });
+        const json = await response.json()
+
+        if (response.ok){
+            dispatch({ type: 'DELETE_WORKOUT', payload: json})
+        }
+
+    }
     return(
-        <div  className='bg-white rounded-lg p-5 my-5 mx-auto relative shadow-lg'>
+        <div  className='bg-white rounded-lg my-5 mx-auto relative shadow-lg flex justify-between'>
+            <div className="p-5">
             <h4 className="text-xl mb-2 text-[#1aac83]">{workout.title}</h4>
             <p className="text-sm text-[#555]"><strong>Load (kg): </strong>{workout.load}</p>
             <p className="text-sm text-[#555]"><strong>Reps (kg): </strong>{workout.reps}</p>
             {workout.createdAt && <p>{workout.createdAt.toString()}</p>}
+            </div>
+            <button onClick={handleDelete} className="flex items-center justify-center rounded-full my-14 mx-5 px-3 py-1 text-sm bg-[#f1f1f1] hover:my-12">Delete</button>
+
+        
 
         </div>
     )
