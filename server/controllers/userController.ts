b7cdc7f5752b7d102e4080f08db67b7439dfb4c1
NjugunaBrian <bryunnjuguna300@gmail.com>
2024-07-09 20:33:16 +0300

    feat: user signup

--- server/controllers/userController.ts
+++ server/controllers/userController.ts
@@ -1,8 +1,19 @@
 import { Response, Request, NextFunction } from "express";
+import UserModel from '../models/userModel';
 
 //signup controller
 export const signupUser = async(req: Request, res: Response, next: NextFunction) => {
-    res.status(200).json({ message: "Signup user"});
+
+    const { email, password } = req.body;
+
+    try{
+        const user = await UserModel.signup( email, password)
+
+        res.status(200).json({ email, user})
+
+    } catch (error : any){
+        res.status(400).json({ error: error.message})
+    }
 
 }
 
--- server/models/userModel.ts
+++ server/models/userModel.ts
@@ -1,4 +1,14 @@
-import mongoose from 'mongoose';
+import mongoose, { Model } from 'mongoose';
+import bcrypt from 'bcrypt';
+
+interface IUser extends Document{
+    email: string,
+    password: string,
+}
+
+interface userModel extends Model<IUser>{
+    signup( email: string, password: string): Promise<IUser>
+}
 
 const Schema = mongoose.Schema;
 
@@ -12,8 +22,22 @@ const userSchema = new Schema({
         type: String,
         required: true,
     }
-})
+});
 
-const UserModel = mongoose.model("UserModel", userSchema);
+//static signup method
+userSchema.statics.signup = async function (email:string, password:string): Promise<IUser> {
+    const exists = await (this as any).findOne({ email })
+
+    if (exists){
+        throw Error('Email already in use');
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await (this as any).create({ email, password: hashedPassword});
+    return user
+}
+
+const UserModel = mongoose.model<IUser, userModel>("UserModel", userSchema);
 
 export default UserModel;
