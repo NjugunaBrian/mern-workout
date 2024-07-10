cfaadb2ad41d19a3650b3d8a06ee01ed064df1a1
NjugunaBrian <bryunnjuguna300@gmail.com>
2024-07-10 14:02:57 +0300

    feat: jwt tokens for signup

--- server/controllers/userController.ts
+++ server/controllers/userController.ts
@@ -1,5 +1,10 @@
 import { Response, Request, NextFunction } from "express";
 import UserModel from '../models/userModel';
+import jwt from "jsonwebtoken";
+
+const createToken = (_id: any) => {
+    return jwt.sign({ _id }, process.env.SECRET!, { expiresIn: '3d'})
+}
 
 //signup controller
 export const signupUser = async(req: Request, res: Response, next: NextFunction) => {
@@ -9,7 +14,11 @@ export const signupUser = async(req: Request, res: Response, next: NextFunction)
     try{
         const user = await UserModel.signup( email, password)
 
-        res.status(200).json({ email, user})
+        //create a token
+        const token = createToken(user._id);
+
+
+        res.status(200).json({ email, token})
 
     } catch (error : any){
         res.status(400).json({ error: error.message})
--- server/models/userModel.ts
+++ server/models/userModel.ts
@@ -1,7 +1,9 @@
 import mongoose, { Model } from 'mongoose';
 import bcrypt from 'bcrypt';
+import validator from "validator";
 
 interface IUser extends Document{
+    _id: any;
     email: string,
     password: string,
 }
@@ -26,6 +28,19 @@ const userSchema = new Schema({
 
 //static signup method
 userSchema.statics.signup = async function (email:string, password:string): Promise<IUser> {
+
+    //validation
+    if(!email || !password){
+        throw Error("All fields must be filled.")
+    }
+
+    if (!validator.isEmail(email)){
+        throw Error("Email is not valid");
+    }
+
+    if (!validator.isStrongPassword(password)){
+        throw Error("Password is not strong enough");
+    }
     const exists = await (this as any).findOne({ email })
 
     if (exists){
